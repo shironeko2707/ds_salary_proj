31063d3b56a6f42edd6d477e601348eccadaa7ce
M-T Duong <74919082+shironeko2707@users.noreply.github.com>
2022-10-04 15:49:47 +0700

    update

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,4 +7,49 @@ Created on Mon Oct  3 09:36:32 2022
 
 import pandas as pd
 
-df = pd.read_csv("./data_scientist.csv")
+df = pd.read_csv("glassdoor_jobs.csv")
+#salary parsing
+#Company name text only
+#state field
+df['hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+df['employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer provided salary:' in x.lower() else 0)
+df = df[df['Salary Estimate'] != '-1']
+salary = df['Salary Estimate'].apply(lambda x: x.split('(')[0])
+minus_K= salary.apply(lambda x: x.replace('K','').replace('$',''))
+
+min_hr= minus_K.apply(lambda x: x.lower().replace('per hour','').replace('employer provided salary:',''))
+df['min_salary'] = min_hr.apply(lambda x: int(x.split('-')[0]))
+df['max_salary'] = min_hr.apply(lambda x: int(x.split('-')[1]))
+df['avg_salary'] = (df.min_salary+df.max_salary)/2
+
+df['company_txt'] = df.apply(lambda x: x['Company Name'] if x['Rating'] < 0 else x['Company Name'][:-3], axis = 1)
+
+df['job_state'] = df['Location'].apply(lambda x: x.split(',')[1])
+
+df['same_state'] = df.apply(lambda x : 1 if x.Location == x.Headquarters else 0 , axis =1)
+
+df['age'] = df.Founded.apply(lambda x: x if x < 1 else 2020 - x)
+
+df['python_yn'] = df['Job Description'].apply(lambda x: 1 if 'python' in x.lower() else 0)
+ 
+#r studio 
+df['R_yn'] = df['Job Description'].apply(lambda x: 1 if 'r studio' in x.lower() or 'r-studio' in x.lower() else 0)
+df.R_yn.value_counts()
+
+#spark 
+df['spark'] = df['Job Description'].apply(lambda x: 1 if 'spark' in x.lower() else 0)
+df.spark.value_counts()
+
+#aws 
+df['aws'] = df['Job Description'].apply(lambda x: 1 if 'aws' in x.lower() else 0)
+df.aws.value_counts()
+
+#excel
+df['excel'] = df['Job Description'].apply(lambda x: 1 if 'excel' in x.lower() else 0)
+df.excel.value_counts()
+
+df.columns
+
+df_out = df.drop(['Unnamed: 0'], axis =1)
+
+df_out.to_csv('salary_data_cleaned.csv',index = False)
